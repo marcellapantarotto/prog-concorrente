6761e8d10a32b0913bda581b7fc878c6887e9ed5
marcellapantarotto <marcellapantarotto@gmail.com>
2020-11-12 19:08:56 -0300

    jantar dos filosofos

--- filosofos.c
+++ filosofos.c
@@ -1,3 +1,12 @@
+// Marcella Pantarotto (13/0143880)
+
+/*
+The problem is defined as follows: There are 5 philosophers sitting at a round table. Between each adjacent pair of philosophers is a chopstick.
+In other words, there are five chopsticks. Each philosopher does two things: think and eat. The philosopher thinks for a while, and then stops
+thinking and becomes hungry. When the philosopher becomes hungry, he/she cannot eat until he/she owns the chopsticks to his/her left and right.
+When the philosopher is done eating he/she puts down the chopsticks and begins thinking again.
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
@@ -9,33 +18,38 @@
 #define ESQ(id) (id)		// talher da esquerda
 #define DIR(id)	(id+1)%N	// talher da direita
 
-sem_t s[N];		// semáforo para os telheres
+#define PENSANDO 0
+#define FAMINTO 1
+#define COMENDO 2
+int estados[N];
+
+sem_t s[N];		// semáforo para os talheres
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void *filosofos (void *arg);
-
 void pega_talher (int n);		// funções executadas pelos filósofo
 void devolve_talher (int n);	// funções executadas pelos filósofo
+void comer (int n);		// funções executadas pelos filósofo
 
 int main () {
 	int i;
 	int *id;
 	//semaforo 
 	for (i=0; i<N; i++) {
-		sem_init(&s[i], 0, 1);		// inicialização semáforo; apenas 1 permissão pq cada um acessa 1 talher
+		sem_init(&s[i], 0, 0);		// inicialização semáforo
 	}
 	pthread_t r[N]; 
 
 	//criacao das threads de filosofos
-       for (i = 0; i < N ; i++) {
-			id = (int *) malloc(sizeof(int));
-			*id = i;
-			pthread_create(&r[i], NULL, filosofos, (void*) (id));
+    for (i = 0; i < N ; i++) {
+		id = (int *) malloc(sizeof(int));
+		*id = i;
+		pthread_create(&r[i], NULL, filosofos, (void*) (id));
 	}
 
-        pthread_join(r[0],NULL);
+    pthread_join(r[0],NULL);
 	return 0;
 }
-
 
 void *filosofos (void *arg) {
 	int n = *((int *) arg);
@@ -55,7 +69,7 @@ void *filosofos (void *arg) {
 }
 
 void pega_talher (int n) {		// n = id do filófoso
-	// if(n == 4){
+	/*
 	if(n/2 == 0){
 		sem_wait(&s[DIR(n)]);
 		sleep(1);
@@ -64,10 +78,27 @@ void pega_talher (int n) {		// n = id do filófoso
 		sem_wait(&s[ESQ(n)]);
 		sleep(1);
 		sem_wait(&s[DIR(n)]);
-	} 
+	} */
+
+	pthread_mutex_lock(&mutex);		// pega lock para mudar estado
+		estados[n] = FAMINTO;
+		comer(n);
+	pthread_mutex_unlock(&mutex);	// libera lock
+	sem_wait(&s[n]);	// decrementa semáforo para talher ficar livre
 }
 
 void devolve_talher (int n) {
-	sem_post(&s[ESQ(n)]);
-	sem_post(&s[DIR(n)]);
+	pthread_mutex_lock(&mutex);		// pega lock para mudar estado
+		estados[n] = PENSANDO;
+		comer(ESQ(n));	// fazer vizinho comer
+		comer(DIR(n));	// fazer vizinho comer
+	pthread_mutex_unlock(&mutex);	// libera lock
+}
+
+void comer (int n) {
+	// testar se pode comer
+	if(estados[n] == FAMINTO && estados[ESQ(n)] != COMENDO && estados[DIR(n)] != COMENDO) {
+		estados[n] = COMENDO;
+		sem_post(&s[n]);	// incrementa semáforo pra indicar que pegou talher
+	}
 }
